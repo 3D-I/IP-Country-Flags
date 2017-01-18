dba04f0c1359edd2999654c3ec0b162099495e71
3Di <three3di@hotmail.it>
2017-01-18 06:44:04 +0100

    Growing up to 1.0.0-b1

--- ext.php
+++ ext.php
@@ -2,7 +2,7 @@
 /**
 *
 * @package phpBB Extension - IPCF 1.0.0-(IP Country Flag)
-* @copyright (c) 2005 - 2008 - 2016 3Di (Marco T.)
+* @copyright (c) 2005 - 2008 - 2017 - 3Di, http://3di.space/32/
 * @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
@@ -10,14 +10,45 @@
 namespace threedi\ipcf;
 
 /**
-* @ignore
-*/
-
+ * IPCF Extension base
+ */
 class ext extends \phpbb\extension\base
 {
+	/**
+	 * Check whether or not the extension can be enabled.
+	 *
+	 * @return bool ||
+	 */
 	public function is_enableable()
 	{
-		$config = $this->container->get('config');
-		return version_compare($config['version'], '3.1.9', '>=');
+		/* @return bool */
+		$bb3110 = ( phpbb_version_compare(PHPBB_VERSION, '3.1.10', '>=') && phpbb_version_compare(PHPBB_VERSION, '3.2.0@dev', '<') );
+
+		/* @return bool */
+		$bb320 = ( phpbb_version_compare(PHPBB_VERSION, '3.2.0', '>=') );
+
+		/**
+		 * We rely on constants.php
+		 */
+		if ( ($bb320) || ($bb3110) )
+		{
+			return true;
+		}
+		else
+		{
+			SELF::verbose_it();
+		}
+	}
+
+	/**
+	 * Let's tell the user what exactly is going on and provide a back-link.
+	 * Using the User Object for BC.
+	 */
+	function verbose_it()
+	{
+		$this->container->get('user')->add_lang_ext('threedi/ipcf', 'ext_require');
+
+		trigger_error($this->container->get('user')->lang['EXTENSION_REQUIREMENTS_NOTICE'] . adm_back_link(append_sid('index.' . $this->container->getParameter('core.php_ext'), 'i=acp_extensions&amp;mode=main')), E_USER_WARNING);
 	}
 }
+
